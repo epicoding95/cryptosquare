dc064d37f485a1069e84f23eea417a8a7928b693
Joseph Tubridy and kelar crisp <kelar@>
2019-06-13 15:47:38 -0700

    initial commit for outline of idea

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+
+var string = ('input#phrase');
+var openArray = string.split('');
+var codedArray = [];
+var square = square root input phrase
+var col# = square
+
+function crypto(string){
+  for ( i = 0; i<openArray.length; i+(col))
+}
+
+$(document).ready(function() {
+  $('form').submit(function(event) {
+    event.preventDefault();
+    var output = convertToRoman($("input#number").val());
+    $("#output").text(output);
+  });
+});
+
+
+
+//     for (var i of Object.keys(roman)) {
+//
+//       var q = Math.floor(num / roman[i]);
+//
+//       num -= q * roman[i];
+//
+//       str += i.repeat(q);
+//     }
+//
+//     return str;
+//   }
